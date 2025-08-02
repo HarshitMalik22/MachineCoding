e075286ffef4dd7942f9f3554527fb6bfbb41613
Harshit <harshitmalik29@gmail.com>
2025-08-02 21:46:17 +0530

    pagination completeted

--- app/pagination/src/App.jsx
+++ app/pagination/src/App.jsx
@@ -3,9 +3,10 @@ import "./App.css";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products");
+    const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
 
     if (data && data.products) {
@@ -17,11 +18,18 @@ export default function App() {
     fetchProducts();
   }, []);
 
+  const selectPageHandler = (selectedPage) => {
+    if(selectedPage >= 1 &&
+      selectedPage <= products.length/10 &&
+      selectedPage !== page) 
+    setPage(selectedPage)
+  };
+
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((prod) => {
+          {products.slice(0, page*10).map((prod) => {
             return(
               <span className="products__single" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
@@ -29,6 +37,26 @@ export default function App() {
               </span>
           );
           })}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className ="pagination">
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick= {() => selectPageHandler(page-1)}>⬅️</span>
+          {[...Array(products.length / 10)].map((_, i) => {
+            return (
+            <span
+            className={page === i+1? "pagination__selected" : ""} 
+            onClick= {() => selectPageHandler(i+1)}
+            key={i}>
+              {i+1}
+            </span>
+            );
+          })}
+          <span
+            onClick= {() => selectPageHandler(page+1)}
+            className={page < products.length/10 ? "" : "pagination__disable"} >➡️</span>
         </div>
       )}
     </div>
